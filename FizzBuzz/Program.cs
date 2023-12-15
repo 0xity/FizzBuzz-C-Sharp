c3f02650f667a1457558f794939f8d6a5844963e
Oxity <oxity.productions@gmail.com>
2023-12-15 23:34:18 +0100

    New expandable design

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System.Data;
+using System.Collections.Generic;
+
+string output;
+Dictionary<int, string> values = new Dictionary<int, string>();
+values.Add(3, "Fizz");
+values.Add(5, "Buzz");
+output = "";
 
 for (int i = 1; i <= 100; i++)
 {
-    if (i % 15 == 0)
+    foreach(KeyValuePair<int, string> fbp in values)
     {
-        Console.WriteLine($"{i} FizzBuzz!");
+        if (i % fbp.Key == 0)
+        {
+            output += fbp.Value;
+        }
     }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine($"{i} Fizz!");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine($"{i} Buzz!");
-    }
-    else Console.WriteLine(Convert.ToString(i));
+    Console.WriteLine($"{i} {output}");
+    output = "";
 }
